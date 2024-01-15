dfc9ded087a555e782d6011adf830502c445eda9
Igor Correa Trifilio Campos <igorcorreatrifilioc@gmail.com>
2024-01-15 17:12:15 -0300

    mudanças no artigo

--- docs/html/artigoData/script/script.js
+++ docs/html/artigoData/script/script.js
@@ -1,10 +1,24 @@
-let tome = document.getElementById('tomas')
+let tome = document.getElementById('tomas');
+const val = tome.value; 
 
-tome.addEventListener('click', function () {
-    let honr = document.getElementById('hidden')
+const scroll= (e)=>{
+    document.querySelector(e).scrollIntoView({behavior:"smooth"});
+}
+
+tome.addEventListener('click', ()=>{
+    let honr = document.querySelector('.hidden')
     honr.classList.toggle('active')
     
-    if (honr.contains('active')) {
-        honr.classList.toggle('inactive')
+    if (honr.classList.contains('active')) 
+    {
+        tome.value='Voltar ao topo'
+        scroll('.descer')
     }
+    else
+    {
+        tome.value=val;
+        scroll('.main')
+    }
+    
+
 })
