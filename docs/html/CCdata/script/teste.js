efad75aa2a515811671f8df9877df8deb99cb031
Igor Correa Trifilio Campos <igorcorreatrifilioc@gmail.com>
2024-02-21 18:35:43 -0300

    Update teste.js

--- docs/html/CCdata/script/teste.js
+++ docs/html/CCdata/script/teste.js
@@ -1,15 +1,1 @@
-function fatorial(num) {
-  let fat = num;
-  if (num % 1 != 0) return gamma(num + 1);
-  if (num == 0 || num == 1) return 1;
 
-  for (let i = num - 1; i >= 1; i--) {
-    fat *= i;
-    if (fat == Infinity) return Infinity;
-  }
-
-  return fat;
-}
-
-
-console.log(eval("Math.sqrt((Math.pow(Math.cos(30)),2))"));
