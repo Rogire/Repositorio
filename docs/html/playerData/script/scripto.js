1e098581f05624a36829db6b8529637f30b959ab
Rogire <igorcorreatrifilioc@gmail.com>
2024-03-02 12:47:55 -0300

    update

--- docs/html/playerData/script/scripto.js
+++ docs/html/playerData/script/scripto.js
@@ -19,25 +19,31 @@ let musicas = [
   },
 ];
 
-//Variáveis
-
-const mus = document.querySelector('audio');
-let indexMusica = 0;
-const fim = document.querySelector('#fim');
-const imagem = document.querySelector('img#img');
-const nomemus = document.querySelector('.descmusic h2');
-const nomeart = document.querySelector('.descmusic p');
-renderizarMusica(indexMusica);
-
-const botaoplay = document.querySelector('.botao-play');
-const botaopau = document.querySelector('.botao-pause');
-const botvoltar = document.querySelector('.anterior')
-const botfrente= document.querySelector('.proximo')
-
+//
+    let indexMusica = 0;
+    const mus = document.querySelector('audio');
+    const rMus = document.querySelector('input.RangeMus');
+    const fim = document.querySelector('#fim');
+    const imagem = document.querySelector('img#img');
+    const nomemus = document.querySelector('.descmusic h2');
+    const nomeart = document.querySelector('.descmusic p');
+    const botaoplay = document.querySelector('.botao-play');
+    const botaopau = document.querySelector('.botao-pause');
+    const botvoltar = document.querySelector('.anterior');
+    const botfrente= document.querySelector('.proximo');
+    const botVol = document.querySelector('.botao-vol')
+    const rangeVol = document.querySelector("#volume");
+    renderizarMusica(indexMusica);
+ 
 // eventos
+botVol.addEventListener("click",   ()=> rangeVol.classList.toggle('active'));
+rMus.addEventListener('change',    ()=> mus.currentTime = (mus.duration*rMus.value)/100);
+
 botaoplay.addEventListener('click', tocarMusica);
 botaopau.addEventListener('click', pausarMusica);
 mus.addEventListener('timeupdate',attBarra);
+rangeVol.addEventListener("change", volumeChange);
+
 
 // funções e arrow funcion
 botvoltar.addEventListener('click', () => {
@@ -68,6 +74,7 @@ function renderizarMusica(index) {
         imagem.src = musicas[index].img;
         fim.textContent = converter(Math.floor(mus.duration));
     });
+    tocarMusica();
 }
 function tocarMusica() {
     mus.play();
@@ -79,17 +86,47 @@ function pausarMusica() {
     botaopau.style.display = 'none'
     botaoplay.style.display = 'flex'
 }
-
+function volumeChange(){
+    if(rangeVol.value == 0)
+    {
+        botVol.classList.remove('fa-volume-high');
+        botVol.classList.remove("fa-volume-low");
+        botVol.classList.add('fa-volume-xmark');
+    }
+    else if(rangeVol.value>0 && rangeVol.value<0.6)
+    {
+        botVol.classList.remove('fa-volume-high');
+        botVol.classList.remove('fa-volume-xmark');
+        botVol.classList.add('fa-volume-low')
+    }
+    else if(rangeVol.value>=0.6)
+    {
+        botVol.classList.remove("fa-volume-xmark");
+        botVol.classList.remove("fa-volume-low");
+        botVol.classList.add("fa-volume-high");
+    }
+    mus.volume = rangeVol.value;
+}
 function attBarra() {
     let tempodec = document.querySelector('#inicio')
     tempodec.textContent = converter(Math.floor(mus.currentTime));
-
     let barra = document.querySelector('progress');
-    barra.style.width = Math.floor((mus.currentTime / mus.duration) * 100) + '%';
+    let pAt = mus.currentTime / mus.duration;
+    barra.style.width = Math.floor(pAt*100) + '%';
+    
+    if(acabou())
+    {
+        if(indexMusica<musicas.length)
+            indexMusica++;
+        else
+            indexMusica=0;
 
-
+        renderizarMusica(indexMusica);
+    }
 }
-
+const acabou = ()=>{
+    return ((mus.currentTime/mus.duration)== 1)?true:false;
+}
 function converter(segundos){
     let cmin = Math.floor(segundos / 60)
     let cseg = segundos % 60;
