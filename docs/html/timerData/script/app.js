d3c4867196d2737098abede04a90fc2deb5f8532
Igor Correa Trifilio Campos <igorcorreatrifilioc@gmail.com>
2025-03-05 13:01:57 -0300

    Polished timer code

--- docs/html/timerData/script/app.js
+++ docs/html/timerData/script/app.js
@@ -1,8 +1,11 @@
 const hora = document.querySelector(".hora");
 const min = document.querySelector(".min");
 const seg = document.querySelector(".seg");
-const PP = document.querySelector("button#PP");
-const add = document.querySelector("button#addTime");
+
+const TimeElements = [hora, min, seg]
+
+const initTimerBtn = document.querySelector("button#PP");
+const addNewTimer = document.querySelector("button#addTime");
 const newT = document.querySelector("button#addNewTimer");
 const listTimers = document.querySelector("div.CLeft");
 const opts = document.querySelectorAll('[opt=optSel]');
@@ -15,163 +18,170 @@ const alarm = document.createElement("audio");
 let hrV="00";
 let minV="00";
 let segV="00";
+const Values = [hrV, minV, segV];
+
+let vInit = [];
 let timer;
 let vef;
+
 alarm.src = opts[0].value;
-//}
 
-hora.addEventListener("change",()=>{
-    if(hora.value.length >2 || hora.value < 0)
-        hora.value = "00";
 
-    hrV =hora.value;
-})
-min.addEventListener("change", () => {
-  if (min.value.length > 2 || min.value < 0 || min.value > 60)
-    min.value = "00";
+hora.addEventListener("change",parseSetTime(TimeElements, true))
+min.addEventListener("change", parseSetTime(TimeElements, true))
+seg.addEventListener("change", parseSetTime(TimeElements, true))
 
-  minV =min.value;
-});
-seg.addEventListener("change", () => {
-  if (seg.value.length > 2 || seg.value < 0 || seg.value > 60)
-    seg.value = "00";
-
-  segV =seg.value;
-});
-PP.addEventListener("click",()=>{
-    if(PP.classList.contains("inactive"))
+/*
+Args:
+elements: array with the time elements in this order: hour, minutes, seconds
+ref: boolean to if its needed to change the Values array
+*/
+function parseSetTime(elements, ref)
+{
+  for(i=0; i<elements.length; i++)
+  {
+    if(i==0)
     {
-            PP.classList.remove("inactive");
-            PP.classList.add("active");
-            PP.textContent = "Pausar";
-            if (seg.value >= 1) seg.value--;
-            if (seg.value < 10 && seg.value.length == 1)
-            seg.value = "0" + seg.value;
-            
-            vef=false;
+      if (elements[i].value < 0 || elements[i].value == "") 
+        elements[i].value = "00";
+    }
+    else 
+      if(elements[i].value.length > 2 || elements[i].value < 0 || elements[i].value > 60 || elements[i].value == "")
+        elements[i].value = "00";
+
+    if(ref)
+      Values[i] = elements[i].value;
+  }
+}
+
+initTimerBtn.addEventListener("click",()=>{
+    if(initTimerBtn.classList.contains("inactive"))
+    {
+            vef = false;
+            initTimerBtn.classList.remove("inactive");
+            initTimerBtn.classList.add("active");
+            initTimerBtn.textContent = "Pausar";
+
+            formatar(false);
+
             timer = setInterval(function time() {
-            if(seg.value >=1)
+            if(seg.value >=0)
                 seg.value--;
             if (seg.value < 10 && seg.value.length == 1)
               seg.value = "0" + seg.value;
 
             document.title = `${hora.value}:${min.value}:${seg.value}`;
             
-            if (seg.value == 0) 
-            {
-                if(min.value>0)
-                {
-                    min.value--;
-                    seg.value = 59;
-                    formatar();
-                }
-                else if(min.value == 0 && hora.value>0)
-                {
-                  if(seg.value == 0)
-                  {
-                    hora.value--;
-                    seg.value = 59;
-                    min.value = 59;
-                    formatar();
-                  }
-                  else
-                  {
-                    hora.value--;
-                    min.value= 59;
-                    formatar();
-                  }
-                }
-                else   
-                    formatar();
-            }
+            if (seg.value < 0) 
+              formatar(true)
+            
             }, 1000);
     }
-    else if(PP.classList.contains("active"))
+    else if(initTimerBtn.classList.contains("active"))
     {
-        PP.classList.remove("active");
-        PP.classList.add("inactive");
-        PP.textContent = "Iniciar";
+        initTimerBtn.classList.remove("active");
+        initTimerBtn.classList.add("inactive");
+        initTimerBtn.textContent = "Iniciar";
         document.title = "Timer";
         clearInterval(timer);
     }
 });
-add.addEventListener("click",()=>{
+addNewTimer.addEventListener("click",()=>{
     if (min.value < 59 && Number(Number(min.value) + 5)<59)
             min.value = Number(Number(min.value) + 5);
     if(min.value.length <2) min.value = '0'+min.value;
 });
-const formatar=()=>{
-    if (min.value == 0 && hora.value > 0) hora.value--;
-    if (hora.value < 10 && hora.value.length == 1) hora.value = "0" + hora.value;
-    if (min.value < 10 && min.value.length == 1) min.value = "0" + min.value;
 
-    if(min.value>60) min.value = "00";
-    if(seg.value>60) seg.value = "00";
+const formatar = (zerou)=>{
+
+  if (min.value > 0 && zerou) 
+    {
+      min.value--;
+      seg.value = 59;
+    } 
+    else if (min.value == 0 && hora.value > 0) 
+    {
+      if (seg.value == 0) 
+      {
+        hora.value--;
+        seg.value = 59;
+        min.value = 59;
+      } 
+      else 
+      {
+        hora.value--;
+        min.value = 59;
+      }
+    }
+
+    if (min.value == 0 && hora.value > 0) 
+      hora.value--;
+
+    if (hora.value < 10 && hora.value.length == 1) 
+      hora.value = "0" + hora.value;
+    
+    if (min.value < 10 && min.value.length == 1) 
+      min.value = "0" + min.value;
+
+    if(min.value>60 || min.value == "")
+       min.value = "00";
+
+    if(seg.value>60 || seg.value == "") 
+      seg.value = "00";
     
     if (hora.value == 0 && min.value == 0 && seg.value == 0) 
     {
       clearInterval(timer);
-      PP.classList.remove("active");
-      PP.classList.add("inactive");
-      PP.textContent = "Iniciar";
+      initTimerBtn.classList.remove("active");
+      initTimerBtn.classList.add("inactive");
+      initTimerBtn.textContent = "Iniciar";
       document.title = "Timer";
       acabou();
     }
 }
+
+// Add new timer to the timer list
 newT.addEventListener("click",()=>{
     document.body.style.background =
       "linear-gradient(to bottom,rgba(10, 26, 56,0.9),rgba(63, 37, 21, 0.9))";
-    //cria div container geral
+    //create the general container div
     let setNT=document.createElement("div");
-    //cria div container dos inputs e inputs
+    //create the inputs and buttons container div
     let tNewT = document.createElement("div");
+    //create inputs
     let inpHrNT= document.createElement("input");
     let inpMinNT = document.createElement("input");
     let inpSegNT = document.createElement("input");
-    //cria div container dos botoes e os botoes 
+    let inpName = document.createElement("input");
+    let TimeInputs = [inpHrNT, inpMinNT, inpSegNT];
+
+    //create buttons and buttons div 
     let btns = document.createElement("div");
     let conf = document.createElement("button");
     let cancel = document.createElement("button");
-    let inpName = document.createElement("input");
-    //configura tipo dos inputs
-    inpHrNT.type = "number";
-    inpMinNT.type = "number";
-    inpSegNT.type = "number";
-    inpName.type = "text";
 
     conf.textContent= "confirmar";
     cancel.textContent= "cancelar";
     inpName.placeholder="Digite o nome do timer";
     inpName.style.margin="3px";
 
-    inpHrNT.value = "00";
-    inpMinNT.value = "00";
-    inpSegNT.value = "00";
-
-    inpHrNT.addEventListener("change", () => {
-      if (inpHrNT.value < 0) inpHrNT.value = "00";
-    });
-    inpMinNT.addEventListener("change", () => {
-      if (inpMinNT.value < 0 || inpMinNT.value > 60) inpMinNT.value = "00";
-    });
-    inpSegNT.addEventListener("change", () => {
-      if (inpSegNT.value < 0 || inpSegNT.value > 60) inpSegNT.value = "00";
-    });
     //configurações de classe dos elementos HTML
     setNT.classList.add("setNT");
     tNewT.style.display = "flex";
     btns.style.display = "flex";
-    inpHrNT.classList.add("tInp");
-    inpMinNT.classList.add("tInp");
-    inpSegNT.classList.add("tInp");
+
+    TimeInputs.forEach((input)=>{
+      input.addEventListener("change", parseSetTime(TimeInputs, null));
+      input.value = "00";
+      input.classList.add("tInp");
+      input.style.fontSize = "2.5em";
+      input.type = "number";
+    })
+
     conf.classList.add("btn");
     cancel.classList.add("btn");
 
-    inpHrNT.style.fontSize="2.5em";
-    inpMinNT.style.fontSize="2.5em";
-    inpSegNT.style.fontSize="2.5em";
-
-    //Append dos elementos nos seus containers e no corpo
+    //Add elements to body
     tNewT.appendChild(inpHrNT);
     tNewT.appendChild(inpMinNT);
     tNewT.appendChild(inpSegNT);
@@ -182,57 +192,58 @@ newT.addEventListener("click",()=>{
     setNT.appendChild(btns);
     document.body.appendChild(setNT);
 
-    cancel.addEventListener("click",()=>{
+    cancel.addEventListener("click",
+    ()=>{
         document.body.removeChild(setNT);
         document.body.style.backgroundColor="transparent";
         document.body.style.background =
           "linear-gradient(to bottom,rgb(10, 26, 56),rgba(63, 37, 21, 0.863))";
     })
 
-    conf.addEventListener("click",()=>{
-        //cria elementos
-        let itenTimer = document.createElement("div");
-        let ContTimer = document.createElement("div");
-        let Hr = document.createElement("div");
-        let Min = document.createElement("div");
-        let Seg = document.createElement("div");
-        let itenTimerName = document.createElement("p");
-        let deleteIten = document.createElement("button");
-        //valores texto dos elementos
+    conf.addEventListener("click",
+    ()=>{
+        // elements to the new div created
+        let itenTimer = document.createElement("div"), ContTimer = document.createElement("div");
+        let Hr = document.createElement("div"), Min = document.createElement("div"), Seg = document.createElement("div");
+        let DivTimeElments = [Hr, Min, Seg];
+        let itenTimerName = document.createElement("p"), deleteIten = document.createElement("button");
 
-        if (inpHrNT.value < 0) inpHrNT.value = "00";
-        if (inpMinNT.value < 0 || inpMinNT.value > 60) inpMinNT.value = "00";
-        if (inpSegNT.value < 0 || inpSegNT.value > 60) inpSegNT.value = "00";
-
+        parseSetTime(TimeInputs, null)
         itenTimerName.textContent = inpName.value;
-        Hr.textContent = inpHrNT.value.length < 2 ? "0" + inpHrNT.value : inpHrNT.value;
-        Min.textContent = inpMinNT.value.length < 2 ? "0" + inpMinNT.value : inpMinNT.value;
-        Seg.textContent = inpSegNT.value.length < 2 ? "0" + inpSegNT.value : inpSegNT.value;
+
+        DivTimeElments.forEach((element, index)=>{
+          element.textContent = TimeInputs[index].value.length < 2 ? "0" + TimeInputs[index].value : TimeInputs[index].value;
+
+          if(!element.classList.contains("iten"))
+            element.classList.add("iten");
+        });
+
         deleteIten.textContent = "excluir";
-        //classe dos elementos
+        //set elements style classes
         itenTimer.classList.add("itenTimer");
         deleteIten.classList.add("btn");
-        Hr.classList.add("iten");
-        Min.classList.add("iten");
-        Seg.classList.add("iten");
         ContTimer.style.display = "flex";
-        //deletar o item da lista
+
+        //delete item list
         deleteIten.addEventListener("click",()=>{
             listTimers.removeChild(itenTimer);
-            hora.value = hrV;
-            min.value = minV;
-            seg.value=segV;
+
+            TimeElements.forEach((element, index)=>
+            {
+              element.value = Values[index]
+            })
         })
 
         itenTimer.addEventListener("click",()=>{
             if(itenTimer.parentElement == listTimers)
             {
-                hora.value = Hr.textContent;
-                min.value = Min.textContent;
-                seg.value = Seg.textContent;
+              TimeElements.forEach((element, index)=>{
+                element.value = DivTimeElments[index].textContent;
+              })
             }
         })
-        //append do elementos em seus containers
+
+        //append elements in their containers
         ContTimer.appendChild(Hr);
         ContTimer.appendChild(Min);
         ContTimer.appendChild(Seg);
@@ -247,12 +258,12 @@ newT.addEventListener("click",()=>{
     })
 })
 
-for(let i=0; i<opts.length;i++)
-{
-    opts[i].addEventListener("click",()=>{
-    alarm.src = opts[i].value;
-    })
-}
+opts.forEach((opt)=>{
+  opt.addEventListener("click",()=>{
+    alarm.src = opt.value
+  })
+});
+
 //Play selected audio button
 playB.classList.add("btn");
 pauseB.classList.add("btn");
@@ -272,10 +283,8 @@ pauseB.addEventListener("click",()=>{
 })
 
 const acabou = ()=>{
-    let endTimer = document.createElement("div");
-    let btns = document.createElement("div");
-    let endBtn = document.createElement("button");
-    let resetBtn = document.createElement("button");
+    let endTimer = document.createElement("div"), btns = document.createElement("div");
+    let endBtn = document.createElement("button"), resetBtn = document.createElement("button");
     let pEnd = document.createElement("p");
 
     const fimTimer = setInterval(function time() {
@@ -286,35 +295,41 @@ const acabou = ()=>{
     pEnd.textContent = "Fim do timer";
     endBtn.textContent = "Confirmar";
     resetBtn.textContent= "Reiniciar timer";
+
     endTimer.classList.add("endTimer")
     endBtn.classList.add("btn");
     resetBtn.classList.add("btn");
     btns.style.display = "flex";
     btns.style.margin = "5px";
+    document.body.style.background =
+      "linear-gradient(to bottom,rgba(10, 26, 56,0.9),rgba(63, 37, 21, 0.9))";
+
     btns.appendChild(endBtn);
     btns.appendChild(resetBtn);
     endTimer.appendChild(pEnd);
     endTimer.appendChild(btns);
-    document.body.style.background =
-      "linear-gradient(to bottom,rgba(10, 26, 56,0.9),rgba(63, 37, 21, 0.9))";
     document.body.appendChild(endTimer);
 
     endBtn.addEventListener("click",()=>{
         document.body.removeChild(endTimer);
         alarm.pause();
         clearInterval(fimTimer);
+
         document.title = "Timer";
         document.body.style.background =
           "linear-gradient(to bottom,rgb(10, 26, 56),rgba(63, 37, 21, 0.863))";
     });
 
     resetBtn.addEventListener("click",()=>{
-        hora.value = hrV;
-        min.value = minV;
-        seg.value = segV;
+      TimeElements.forEach((element, index)=>
+      {
+        element.value = Values[index]
+      })
+
         clearInterval(fimTimer);
-        PP.click();
+        initTimerBtn.click();
         document.body.removeChild(endTimer);
+
         document.body.style.background =
           "linear-gradient(to bottom,rgb(10, 26, 56),rgba(63, 37, 21, 0.863))";
     })
